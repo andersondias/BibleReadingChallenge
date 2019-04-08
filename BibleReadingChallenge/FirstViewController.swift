aa05184150514cda21f548315257b52d7016f3f1
Anderson Dias <andersondaraujo@gmail.com>
2019-04-08 12:37:51 -0300

    Initial Commit

---
+++ BibleReadingChallenge/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BibleReadingChallenge
+//
+//  Created by Anderson Dias on 08/04/19.
+//  Copyright © 2019 Anderson Dias. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BibleReadingChallenge/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BibleReadingChallenge
+//
+//  Created by Anderson Dias on 08/04/19.
+//  Copyright © 2019 Anderson Dias. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
