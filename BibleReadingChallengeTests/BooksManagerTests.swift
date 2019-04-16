30d5d597be0108e9145def2234df16e35d2448b2
Anderson Dias <andersondaraujo@gmail.com>
2019-04-16 09:34:14 -0300

    Improving specs

--- BibleReadingChallengeTests/BooksManagerTests.swift
+++ BibleReadingChallengeTests/BooksManagerTests.swift
@@ -10,9 +10,13 @@ import XCTest
 @testable import BibleReadingChallenge
 
 class BooksManagerTests: XCTestCase {
+    let genesis = BooksManager.shared.books[0]
+    let exodus = BooksManager.shared.books[1]
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        for book in BooksManager.shared.books {
+            book.readChapters.removeAll()
+        }
     }
 
     override func tearDown() {
@@ -21,6 +25,39 @@ class BooksManagerTests: XCTestCase {
 
     func testNumberOfChapters() {
         XCTAssertEqual(BooksManager.shared.numberOfChapters, 1189)
+    }
+    
+    // Increments the number of read chapters when a chapter is read
+    func testReadChapters() {
+        BooksManager.shared.readChapter(book: genesis, chapter: 1)
+        XCTAssertEqual(BooksManager.shared.numberOfReadChapters, 1)
+
+        BooksManager.shared.readChapter(book: genesis, chapter: 2)
+        XCTAssertEqual(BooksManager.shared.numberOfReadChapters, 2)
+        
+        BooksManager.shared.readChapter(book: exodus, chapter: 3)
+        BooksManager.shared.readChapter(book: exodus, chapter: 4)
+        XCTAssertEqual(BooksManager.shared.numberOfReadChapters, 4)
+    }
+    
+    // Do not increment counter when the chapter is already read
+    func testReadChaptersDoNotIncrementsTwice() {
+        BooksManager.shared.readChapter(book: genesis, chapter: 2)
+        BooksManager.shared.readChapter(book: genesis, chapter: 2)
+        XCTAssertEqual(BooksManager.shared.numberOfReadChapters, 1)
+    }
+    
+    // Decrements the number of read chapters when chapter is unred
+    func testUnreadChapter() {
+        BooksManager.shared.readChapter(book: genesis, chapter: 1)
+        BooksManager.shared.unreadChapter(book: genesis, chapter: 1)
+        XCTAssertEqual(BooksManager.shared.numberOfReadChapters, 0)
+    }
+    
+    // Do not decrement already unread chapter
+    func testUnreadChapterWhenItIsAlreadyUnread() {
+        BooksManager.shared.unreadChapter(book: genesis, chapter: 2)
+        XCTAssertEqual(BooksManager.shared.numberOfReadChapters, 0)
     }
 
 }
