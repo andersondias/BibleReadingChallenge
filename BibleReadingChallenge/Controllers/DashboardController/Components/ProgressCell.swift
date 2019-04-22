c7415c72ed253e0cd6505ec54c7b09469e2c2a3a
Anderson Dias <andersondaraujo@gmail.com>
2019-04-21 21:51:25 -0300

    Dashboard redesign

---
+++ BibleReadingChallenge/Controllers/DashboardController/Components/ProgressCell.swift
@@ -0,0 +1,20 @@
+//
+//  ProgressCell.swift
+//  BibleReadingChallenge
+//
+//  Created by Anderson Dias on 21/04/19.
+//  Copyright © 2019 Anderson Dias. All rights reserved.
+//
+
+import UIKit
+
+class ProgressCell: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        
+        super.draw(rect)
+        
+    }
+
+}
