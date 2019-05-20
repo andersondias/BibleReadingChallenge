b47483e9fc35776e20b839d73e3b53b7a960c6e7
Anderson Dias <andersondaraujo@gmail.com>
2019-05-20 10:17:05 -0300

    Reading statistics

---
+++ BibleReadingChallenge/Shared/ReadingStatistics.swift
@@ -0,0 +1,11 @@
+//
+//  ReadingStatistics.swift
+//  BibleReadingChallenge
+//
+//  Created by Anderson Dias on 17/05/19.
+//  Copyright © 2019 Anderson Dias. All rights reserved.
+//
+
+class ReadingStatistics {
+    
+}
